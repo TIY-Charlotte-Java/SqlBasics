c00b6034d0898cc79c300e02bff9aa3fe294237b
crenny288 <crenny288@gmail.com>
2016-04-21 16:55:25 -0400

    2nd pass. Got the server working, 404 Not Found

--- src/com/theironyard/clt/Main.java
+++ src/com/theironyard/clt/Main.java
@@ -27,10 +27,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        user.editMessage();
-        user.createMessage();
-        user.addMessage();
-        user.deleteMessage();
+//        user.editMessage();
+//        user.createMessage();
+//        user.addMessage();
+//        user.deleteMessage();
         
 
         Spark.init();
